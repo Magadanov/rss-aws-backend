bb0bbe3a2f082885b0b2a17d71df69e0473d3dae
Magadanov <magadanov03@mail.ru>
2025-03-04 19:52:12 +0500

    feat: add validation function

--- product_service/lambda/createProduct.js
+++ product_service/lambda/createProduct.js
@@ -3,6 +3,32 @@ const AWS = require("aws-sdk");
 const { getCorsHeaders } = require("./httpHeader");
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
+function validateProduct(body) {
+    const errors = [];
+
+    if (
+        !body.title ||
+        typeof body.title !== "string" ||
+        body.title.trim() === ""
+    ) {
+        errors.push("Title is required and should not be empty.");
+    }
+
+    if (typeof body.description !== "string") {
+        errors.push("Description should be a string.");
+    }
+
+    if (typeof body.price !== "number" || body.price <= 0) {
+        errors.push("Price should be a positive number greater than 0.");
+    }
+
+    if (typeof body.count !== "number" || body.count <= 0) {
+        errors.push("Count should be a positive number greater than 0.");
+    }
+
+    return errors;
+}
+
 exports.handler = async (event) => {
     console.log("createProductEvent:", JSON.stringify(event, null, 2));
 
@@ -14,19 +40,13 @@ exports.handler = async (event) => {
                 ? JSON.parse(event?.body)
                 : event?.body;
 
-        if (
-            !body.title ||
-            !body.price ||
-            !body.description ||
-            body.count === undefined
-        ) {
+        const errors = validateProduct(body);
+
+        if (errors.length > 0) {
             return {
                 statusCode: 400,
                 headers,
-                body: JSON.stringify({
-                    message:
-                        "Missing required fields: title, price, description, count",
-                }),
+                body: JSON.stringify({ message: errors.join(" ") }),
             };
         }
 
